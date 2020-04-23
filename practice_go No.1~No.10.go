1652d5bbed779299874ac9340d092e5cd0689f0e
shotasan <wondermoai@gmail.com>
2020-04-23 22:04:05 +0900

    No.5

--- practice_go No.1~No.10.go
+++ practice_go No.1~No.10.go
@@ -102,3 +102,46 @@ func Factorial(n int) int {
   
   return n * Factorial(n - 1)
 }
+
+// No.5
+// In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+// The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+// Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+// You have to write a function printer_error which given a string will output the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+// The string has a length greater or equal to one and contains only letters from ato z.
+// #Examples:
+// s="aaabbbbhaijjjm"
+// error_printer(s) => "0/14"
+// s="aaaxbbbbyyhwawiwjjjwwm"
+// error_printer(s) => "8/22"
+
+// My
+package kata
+
+import (
+  "regexp"
+  "strconv"
+)
+
+func PrinterError(s string) string {
+  re := regexp.MustCompile(`[n-z]`)
+  badStrings := re.FindAllString(s, -1)
+  badNumbers := len(badStrings)
+  totalNumbers := len([]rune(s))
+  return strconv.Itoa(badNumbers) + "/" + strconv.Itoa(totalNumbers)
+}
+
+// Best
+package kata
+
+import "fmt"
+
+func PrinterError(s string) string {
+  errors := 0
+  for _, c := range s {
+    if string(c) > "m" {
+      errors++
+    }
+  }
+  return fmt.Sprintf("%d/%d", errors, len(s))
+}
