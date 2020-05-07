d2f6362e3e18db451b0df42d2881c53e5e2336df
shotasan <wondermoai@gmail.com>
2020-05-07 22:09:17 +0900

    No.8

--- practice_go No.1~No.10.go
+++ practice_go No.1~No.10.go
@@ -231,3 +231,31 @@ func Solve(s string) int {
     }
     return max
 }
+
+// No.8
+// Write a function that takes a positive integer n, sums all the cubed values from 1 to n, and returns that sum.
+// Assume that the input n will always be a positive integer.
+// Examples:
+// SumCubes(2) == 9
+// sum of the cubes of 1 and 2 is 1 + 8
+
+// Best
+func SumCubes(n int) int {
+  var sum int
+  for i := 1; i <= n; i++ {
+    sum += i * i * i
+  }
+
+  return sum
+}
+
+// My(false)
+import "math"
+
+func SumCubes(n int) int {
+  var res float64
+  for i := 1; i <= n; i++ {
+    res += math.Pow(float64(i), 3)
+  }
+  return int(res)
+}
