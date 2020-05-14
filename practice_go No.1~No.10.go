d0a3b392b4e274d5cf8411aa50b7be5a531ad72b
shotasan <wondermoai@gmail.com>
2020-05-14 22:26:08 +0900

    No.10

--- practice_go No.1~No.10.go
+++ practice_go No.1~No.10.go
@@ -286,3 +286,23 @@ func OrderedCount(text string) []Tuple {
   }
   return counts 
 }
+
+// No.10
+// Create a function add(n)/Add(n) which returns a function that always adds n to any number
+// Note for Java: the return type and methods have not been provided to make it a bit more challenging.
+// var addOne = Add(1)
+// addOne(3) // 4
+
+// My
+func Add(a int) func(int)int {
+  return func(b int) int {
+    return a + b
+  }
+}
+
+// Best
+func Add(i int) func(int)int {
+  return func(j int) int {
+    return i + j
+  }
+}
