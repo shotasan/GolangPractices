2ea382312b94f901c5a75e16f705b37be974f351
shotasan <wondermoai@gmail.com>
2020-06-05 23:14:50 +0900

    No.14

--- practice_go No.11~No.20.go
+++ practice_go No.11~No.20.go
@@ -102,3 +102,34 @@ func SumEvenFibonacci(limit int) int {
   }
   return sum
 }
+
+// No.14
+// My friend wants a new band name for her band. She like bands that use the formula: "The" + a noun with the first letter capitalized, for example:
+// "dolphin" -> "The Dolphin"
+// However, when a noun STARTS and ENDS with the same letter, she likes to repeat the noun twice and connect them together with the first and last letter, combined into one word (WITHOUT "The" in front), like this:
+// "alaska" -> "Alaskalaska"
+// Complete the function that takes a noun as a string, and returns her preferred band name written as a string.
+
+// My
+import "strings"
+
+func bandNameGenerator(word string) string {
+  if word[:1] != word[len(word)-1:] {
+    return "The " + strings.ToUpper(word[:1]) + word[1:len(word)]
+  }
+  return strings.ToUpper(word[:1]) + word[1:len(word)] + word[1:len(word)]
+}
+
+// Best
+import "strings"
+
+func bandNameGenerator(word string) string {
+  first := word[:1]
+  last := word[len(word)-1:]
+
+  if first != last {
+    return "The " + strings.Title(word)
+  }
+
+  return strings.Title(word) + word[1:]
+}
