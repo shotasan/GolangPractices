155d4e2db530c40f27ed6bdf544d10a903423252
shotasan <wondermoai@gmail.com>
2020-06-09 22:08:14 +0900

    No.16

--- practice_go No.11~No.20.go
+++ practice_go No.11~No.20.go
@@ -166,3 +166,39 @@ func Accum(s string) string {
     
     return strings.Join(parts, "-")
 }
+
+// No.16
+// Friday 13th or Black Friday is considered as unlucky day. Calculate how many unlucky days are in the given year.
+// Find the number of Friday 13th in the given year.
+// Input: Year as an integer.
+// Output: Number of Black Fridays in the year as an integer.
+// Examples:
+// unluckyDays(2015) == 3
+// unluckyDays(1986) == 1
+
+// My
+import "time"
+
+func UnluckyDays(year int) int {
+  count := 0
+  for i := 1; i <= 12; i++ {
+    t := time.Date(year, time.Month(i), 13, 0, 0, 0, 0, time.Local)
+    if t.Weekday().String() == "Friday" {
+      count ++
+    }
+  }
+  return count
+}
+
+// Best
+import "time"
+
+func UnluckyDays(year int) int {
+  tot := 0
+  for m:=1; m<=12; m++ {
+    if time.Date(year, time.Month(m), 13, 0, 0, 0, 0, time.UTC).Weekday() == 5 {
+      tot++
+    }
+  }
+  return tot
+}
