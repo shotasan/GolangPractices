b2ad79b0c77f43e173735231b659a361fd50546f
shotasan <wondermoai@gmail.com>
2020-06-25 23:08:13 +0900

    No.21

--- practice_go No.21~No.30.go
+++ practice_go No.21~No.30.go
@@ -40,3 +40,24 @@ func solve(slice []string) []int {
 
   return result
 }
+
+// No.22
+// Complete the method which returns the number which is most frequent in the given input array. If there is a tie for most frequent number, return the largest number among them.
+// Note: no empty arrays will be given.
+// Examples
+// [12, 10, 8, 12, 7, 6, 4, 10, 12]              -->  12
+// [12, 10, 8, 12, 7, 6, 4, 10, 12, 10]          -->  12
+// [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]  -->   3
+
+// Best
+func HighestRank(nums []int) int {
+  mii, maxK, maxV := map[int]int{}, 0, 0
+  for _, v := range nums {
+    mii[v]++
+    if mii[v] > maxV || (mii[v] == maxV && v > maxK) {
+      maxK = v
+      maxV = mii[v]
+    }
+  }
+  return maxK
+}
