50fca00d1397f0a3afe7d68db7156ce197492fab
shotasan <wondermoai@gmail.com>
2020-06-29 22:32:38 +0900

    No.24

--- practice_go No.21~No.30.go
+++ practice_go No.21~No.30.go
@@ -98,3 +98,41 @@ func CartesianNeighbor(x,y int) [][]int{
   }
   return res
 }
+
+// No.24
+// Complete the function so that it finds the mean of the three scores passed to it and returns the letter value associated with that grade.
+// Numerical Score	Letter Grade
+// 90 <= score <= 100	'A'
+// 80 <= score < 90	'B'
+// 70 <= score < 80	'C'
+// 60 <= score < 70	'D'
+// 0 <= score < 60	'F'
+// Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+
+// My
+func GetGrade(a,b,c int) rune {
+    switch avg := (a + b + c) / 3; {
+      case 90 <= avg && avg <= 100:
+        return 'A'
+      case 80 <= avg && avg < 90:
+        return 'B'
+      case 70 <= avg && avg < 80:
+        return 'C'
+      case 60 <= avg && avg < 70:
+        return 'D'
+      default:
+        return 'F'
+    } 
+}
+
+// Best
+func GetGrade(a, b, c int) rune {
+    switch ((a + b + c) / 30) {
+        case 10: return 'A'
+        case 9: return 'A'
+        case 8: return 'B'
+        case 7: return 'C'
+        case 6: return 'D'
+        default: return 'F'
+    }
+}
