1a1afe9dbf543c3fae07034837f388f6e4777e81
shotasan <wondermoai@gmail.com>
2020-07-01 22:31:04 +0900

    No.25

--- practice_go No.21~No.30.go
+++ practice_go No.21~No.30.go
@@ -136,3 +136,28 @@ func GetGrade(a, b, c int) rune {
         default: return 'F'
     }
 }
+
+// No.25
+// Given the sum and gcd of two numbers, return those two numbers in ascending order. If the numbers do not exist, return -1, (or NULL in C, tuple (-1,-1) in C#, pair (-1,-1) in C++, array {-1,-1} in Java and Golang).
+// For example: 
+// Given sum = 12 and gcd = 4...
+// solve(12,4) = [4,8]. The two numbers 4 and 8 sum to 12 and have a gcd of 4.
+// solve(12,5) = -1. No two numbers exist that sum to 12 and have gcd of 5.
+// solve(10,2) = [2,8]. Note that [4,6] is also a possibility but we pick the one with the lower first element: 2 < 4, so we take [2,8].
+
+// My
+func Solve(s int, g int) []int {
+  sum := s - g
+  if sum % g == 0 {
+    return []int{g, sum}
+  }
+  return []int{-1, -1}
+}
+
+// Best
+func Solve(s int, g int) []int {
+  if (s-g) % g == 0{
+    return []int{g, s-g}
+  }
+  return []int{-1,-1}
+}
